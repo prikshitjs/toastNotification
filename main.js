ae70106bc3ecfdc82270d42a383ca5bdbb9c7de6
Prikshit js <prikshitxcode@gmail.com>
2025-04-21 16:12:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+let shownotif = document.querySelector('.showNotification');
+
+function success(text) {
+	let notif = document.createElement('div');
+	notif.classList.add('notif');
+	notif.classList.add('success');
+	notif.innerHTML = `<i class="fa-solid fa-circle-check"></i> ${text}`;
+	shownotif.appendChild(notif);
+	setTimeout(()=>{
+		notif.remove();
+	},8000)
+}
+function error(text) {
+	let notif = document.createElement('div');
+	notif.classList.add('notif');
+	notif.classList.add('alert');
+	notif.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> ${text}`;
+	shownotif.appendChild(notif);
+	setTimeout(()=>{
+		notif.remove();
+	},8000)
+}
+function invalid(text) {
+	let notif = document.createElement('div');
+	notif.classList.add('notif');
+	notif.classList.add('invalid');
+	notif.innerHTML = `<i class="fa-solid fa-circle-xmark"></i> ${text}`;
+	shownotif.appendChild(notif);
+	setTimeout(()=>{
+		notif.remove();
+	},8000)
+}
